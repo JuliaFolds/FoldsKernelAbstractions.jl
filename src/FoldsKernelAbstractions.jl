ca7e952a6687b46a66ac9a94604f4e956a47605d
Takafumi Arakaki <aka.tkf@gmail.com>
2021-01-17 06:55:03 -0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/FoldsKernelAbstractions.jl
@@ -0,0 +1,5 @@
+module FoldsKernelAbstractions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FoldsKernelAbstractions
+using Test
+
+@testset "FoldsKernelAbstractions.jl" begin
+    # Write your tests here.
+end
